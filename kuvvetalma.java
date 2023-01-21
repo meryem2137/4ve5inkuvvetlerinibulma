d0c9938cd7f858d24ce1fc821086cdbc5851ac00
meryem2137 <116557318+meryem2137@users.noreply.github.com>
2023-01-21 13:25:16 +0300

    Add files via upload

---
+++ kuvvetalma.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class kuvvetalma {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz : ");
+        int n = input.nextInt();
+
+        int a;
+        for(a= 1; a < n; a *= 4) {
+            System.out.println("4'ün Kuvvetleri : " + a);
+        }
+
+        System.out.println("-------------------------------");
+
+        for(a = 1; a < n; a *= 5) {
+            System.out.println("5'in Kuvvetleri : " + a);
+
+
+
+
+    }}}
+
+
+
+
+
+
+
+
+//www.patika.dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
